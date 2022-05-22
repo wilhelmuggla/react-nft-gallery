75cff3bf6864aad180c9e7fb7d15e5ff0e0df6b5
Wilhelm Uggla <willeuggla@gmail.com>
2022-05-22 21:54:29 +0200

    updated container size

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { NftProvider, useNft } from "use-nft"
 import Nft from './Nft';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, GridItem, Grid, Button, Text, FormControl, FormLabel, Input, FormHelperText, Alert, AlertIcon } from '@chakra-ui/react';
+import { Container, GridItem, SimpleGrid, Button, Text, FormControl, FormLabel, Input, FormHelperText, Alert, AlertIcon } from '@chakra-ui/react';
 
 const ethersConfig = {
   provider: getDefaultProvider("homestead"),
@@ -50,7 +50,7 @@ function App() {
 
   return (
     <>
-      <Container>
+      <Container maxWidth={1200}>
         <Text fontSize="4xl" fontWeight="bold" marginBottom="4" textAlign="center">Nft Gallery</Text>
         <FormControl marginBottom={4}>
           <FormLabel fontWeight={700} htmlFor='email'>Contract-address</FormLabel>
@@ -62,13 +62,13 @@ function App() {
             {errorMessageText}
           </Alert>
           : <></>}
-        <Grid templateColumns='repeat(3, 1fr)' gap={6}>
+        <SimpleGrid columns={[2, null, 3]} gap={6}>
           {nfts.length > 0 ? nfts.slice(0, showCount).map((nft, key) =>
             <GridItem key={key} >
               <Nft title={nft.title} address={nft.contract.address} id={parseInt(nft.id.tokenId, 16)} image={nft.media[0].gateway} />
             </GridItem>
           ) : null}
-        </Grid>
+        </SimpleGrid>
       </Container>
       <Container marginTop='4' centerContent>
         {nfts.length > 0 ? <Button align='center' onClick={() => { setShowCount(showCount + perPage) }}>Load more</Button> : <></>}
