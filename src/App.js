3e877afedc4f78190a43f589067537fff643f239
Wilhelm Uggla <willeuggla@gmail.com>
2022-05-22 21:44:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
+import { getDefaultProvider, utils } from "ethers"
+import { NftProvider, useNft } from "use-nft"
+import Nft from './Nft';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, GridItem, Grid, Button, Text, FormControl, FormLabel, Input, FormHelperText, Alert, AlertIcon } from '@chakra-ui/react';
+
+const ethersConfig = {
+  provider: getDefaultProvider("homestead"),
+}
+
+
 
 function App() {
+  const perPage = 9;
+  const [nfts, setNfts] = useState([]);
+  const [showCount, setShowCount] = useState(perPage);
+  const [address, setAddress] = useState('');
+  const [errorMessageText, setErrorMessageText] = useState('');
+  const [startToken, setStartToken] = useState('');
+
+  useEffect(() => {
+    const apiKey = "FBJHuEKR11meXXyZIYay1MMJEA8UI5Bf";
+    const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${apiKey}/getNFTsForCollection`;
+    const withMetadata = true;
+    const perPage = 9;
+    if (!utils.isAddress(address) && address != '') {
+      setErrorMessageText('Invalid address');
+      setNfts(null);
+    } else {
+      if ((nfts == null || nfts.length < showCount) && address != '') {
+        var config = {
+          method: 'get',
+          url: `${baseURL}?contractAddress=${address}&withMetadata=${withMetadata}&startToken=${startToken}`,
+          headers: {}
+        };
+        axios(config)
+          .then(response => {
+            setErrorMessageText('');
+            setNfts([...nfts, ...response.data.nfts]);
+            setStartToken(response.data.nfts[response.data.nfts.length - 1].id.tokenId);
+          })
+          .catch(error => setErrorMessageText(error.message));
+      }
+    }
+  }, [showCount, address]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Container>
+        <Text fontSize="4xl" fontWeight="bold" marginBottom="4" textAlign="center">Nft Gallery</Text>
+        <FormControl marginBottom={4}>
+          <FormLabel fontWeight={700} htmlFor='email'>Contract-address</FormLabel>
+          <Input id='email' type='email' value={address} onChange={(val) => { setAddress(val.target.value) }} />
+        </FormControl>
+        {errorMessageText ?
+          <Alert show={errorMessageText} status='error'>
+            <AlertIcon />
+            {errorMessageText}
+          </Alert>
+          : <></>}
+        <Grid templateColumns='repeat(3, 1fr)' gap={6}>
+          {nfts.length > 0 ? nfts.slice(0, showCount).map((nft, key) =>
+            <GridItem key={key} >
+              <Nft title={nft.title} address={nft.contract.address} id={parseInt(nft.id.tokenId, 16)} image={nft.media[0].gateway} />
+            </GridItem>
+          ) : null}
+        </Grid>
+      </Container>
+      <Container marginTop='4' centerContent>
+        {nfts.length > 0 ? <Button align='center' onClick={() => { setShowCount(showCount + perPage) }}>Load more</Button> : <></>}
+      </Container>
+    </>
+  )
 }
+
+
+
 
 export default App;
---
+++ src/Nft.js
@@ -0,0 +1,26 @@
+import { Box, Image, Link } from '@chakra-ui/react'
+
+function Nft(props) {
+
+
+    // You can now display the NFT metadata.
+    return (
+        <Box maxW='lg'
+            borderWidth='1px' borderRadius='lg' overflow='hidden'
+            mt='1'
+            fontWeight='semibold'
+            as='h4'
+            lineHeight='tight'
+            noOfLines={1}
+        >
+            <Image src={props.image} alt="" />
+            <Link href={"https://opensea.io/assets/" + props.address + '/' + props.id} target="_blank">
+                {props.title ? props.title : "#" +props.id}
+            </Link>
+            {props.description}
+
+        </Box>
+    )
+}
+
+export default Nft;
